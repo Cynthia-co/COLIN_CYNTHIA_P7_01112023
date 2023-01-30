85032556d2be450c1b24e4553ac1e2e8d3257f93
Cynthia-co <cynthiacolin@hotmail.fr>
2023-01-30 17:13:15 +0100

    Updates

---
+++ precache-manifest.1d260e9be44e637f3ddf6455644b90e2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9336ccfda824c27c31",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/css/main.4c87127f.chunk.css"
+  },
+  {
+    "revision": "8e9336ccfda824c27c31",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/js/main.8e9336cc.chunk.js"
+  },
+  {
+    "revision": "3f05954b8f3865a105eb",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/js/1.3f05954b.chunk.js"
+  },
+  {
+    "revision": "3c238283c7aedd9cbdfa",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/js/runtime~main.3c238283.js"
+  },
+  {
+    "revision": "a666dc56791330a1f4ec3305d252c43d",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/homebanner.a666dc56.webp"
+  },
+  {
+    "revision": "e58e9b3c71be3dea66c20d67a85762aa",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/open.e58e9b3c.webp"
+  },
+  {
+    "revision": "0e506a155db3d7c4e830236949aec2cb",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/close.0e506a15.webp"
+  },
+  {
+    "revision": "f185336abb6bcd82058ee8d28e3f08ba",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/bannerimg.f185336a.webp"
+  },
+  {
+    "revision": "efcaaa65c87baf50cd961d705ade9c74",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/backward.efcaaa65.webp"
+  },
+  {
+    "revision": "e5e6a26239baa31b592c6584ebbe06b2",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/forward.e5e6a262.webp"
+  },
+  {
+    "revision": "606d381d58c14591551fb48bd09a37a4",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/star.606d381d.webp"
+  },
+  {
+    "revision": "c938a46bb0a68ca66524775d5af37811",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/redstar.c938a46b.webp"
+  },
+  {
+    "revision": "423c9ed2612b3e0d494b0176bba189e5",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/logo.423c9ed2.webp"
+  },
+  {
+    "revision": "59de14dfb960dbf77898ef24707f4b00",
+    "url": "/COLIN_CYNTHIA_P7_01112023/static/media/logofooter.59de14df.webp"
+  },
+  {
+    "revision": "1ed723506e0d1a4c30f2b866d1bb24bf",
+    "url": "/COLIN_CYNTHIA_P7_01112023/index.html"
+  }
+];
